8f3b4800cdb0ff22852af18f5e7c974d26642ac3
danboyce92 <95984847+danboyce92@users.noreply.github.com>
2022-01-31 21:29:17 +0100

    Add files via upload

---
+++ migrations/2_deploy_proxy.js
@@ -0,0 +1,24 @@
+const Dogs = artifacts.require('Dogs');
+const DogsUpdated = artifacts.require('DogsUpdated');
+const Proxy = artifacts.require('Proxy');
+
+module.exports = async function(deployer, network, accounts) {
+  //Deploy contract
+  const dogs = await Dogs.new();
+  const proxy = await Proxy.new(dogs.address);
+
+  //Create proxy to fool truffle
+  var proxyDog = await Dogs.at(proxy.address);
+  //Set nr of dogs through the proxy
+  await proxyDog.setNumberOfDogs(10);
+  //Tested
+  var nrOfDogs = await proxyDog.getNumberOfDogs();
+  console.log("Before update: " + nrOfDogs.toNumber());
+
+  const dogsUpdated = await DogsUpdated.new();
+  proxy.upgrade(dogsUpdated.address);
+
+  nrOfDogs = await proxyDog.getNumberOfDogs();
+  console.log(nrOfDogs.toNumber());
+
+}
